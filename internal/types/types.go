91d043cc146b12500548cf5a6bef6aad397d9ca3
thenaveensharma <naveenchaurasiya1501@gmail.com>
2025-05-01 22:32:49 +0530

    change student json keys to lowercase

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,8 +1,8 @@
 package types
 
 type Student struct {
-	Id    int64
-	Name  string `validate:"required"`
-	Age   uint8  `validate:"gte=0,lte=130"`
-	Email string `validate:"required,email"`
+	Id    int64  `json:"id"`
+	Name  string `json:"name" validate:"required "`
+	Age   uint8  `json:"age" validate:"gte=0,lte=130"`
+	Email string `json:"email" validate:"required,email"`
 }
